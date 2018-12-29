fceaf0dd72e7c7b3a355c0fbcf4dee9dd89f7db6
saurabhk2399 <saurabh_krishna2009@yahoo.com>
2018-12-29 23:50:09 +0530

    added files

---
+++ JavaScriptAssignment_29Dec.js
@@ -0,0 +1,21 @@
+// var objEmp= {
+
+//     FirstName : "Saurabh",
+//     LastName : "Singh",
+//     Department : "Software"
+
+    
+
+// }
+
+// document.getElementById("demo3").innerHTML = objEmp.FirstName
+
+alert("Test");
+var person = {
+    firstName : "John",
+    lastName  : "Doe",
+    age     : 50,
+    eyeColor  : "blue"
+  };
+  document.getElementById("demo").innerHTML = "Test";
+  document.getElementById("demo").innerHTML = person.firstName + " " + person.lastName;
