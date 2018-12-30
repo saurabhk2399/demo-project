737de8881308cc9d8d84362e4bf77264a26bb277
saurabhk2399 <saurabh_krishna2009@yahoo.com>
2018-12-30 11:51:17 +0530

    added

--- JavaScriptAssignment_29Dec.js
+++ JavaScriptAssignment_29Dec.js
@@ -1,21 +1,55 @@
-// var objEmp= {
 
-//     FirstName : "Saurabh",
-//     LastName : "Singh",
-//     Department : "Software"
-
-    
-
-// }
 
 // document.getElementById("demo3").innerHTML = objEmp.FirstName
 
-alert("Test");
+var objEmp = {
+    firstName: "saurabh",
+    lastName : "Singh",
+    Department     : "IT",
+    fullName : function() {
+    return this.firstName + " " + this.lastName + "" + " " + this.Department;
+}
+};
+
+
 var person = {
-    firstName : "John",
-    lastName  : "Doe",
-    age     : 50,
-    eyeColor  : "blue"
+    firstName: "John",
+    lastName : "Doe",
+    basicsalary     : 4000,
+    totalsal : function() {
+      return this.basicsalary + 4000*12/100 + 4000*2/100;
+    }
   };
-  document.getElementById("demo").innerHTML = "Test";
-  document.getElementById("demo").innerHTML = person.firstName + " " + person.lastName;
+
+
+//   WAY OF ARRAY CREATION
+  var cars = ["bmw", "aUDI", "BMW"];
+  var cars2 = [
+    "Saab",
+    "Volvo",
+    "BMW"
+  ];
+
+  var car4 = "Saab";
+var car5 = "Volvo";
+var car6 = "BMW";
+
+
+// var objEmp2 = {
+//     firstName: "saurabh",
+//     empid= "5659",
+//     basicsalary : "4000",
+    
+//     totalsal : function() {
+//     return this.basicsalary ;
+// }
+// 
+
+// var person = {
+//     firstName: "John",
+//     lastName : "Doe",
+//     id     : 5566,
+//     fullName : function() {
+//       return this.id + 55;
+//     }
+//   };
